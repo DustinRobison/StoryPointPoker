2ac5720c7dcfd1bf1ef5e745cda0cda0655788af
Dustin Robison <dustinrobi@gmail.com>
2023-05-12 10:55:50 -0700

    chore: added functions but WIP

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,24 @@
-import * as functions from "firebase-functions";
+import { differenceInMinutes } from "date-fns";
+import admin from "firebase-admin";
+import { logger } from "firebase-functions";
+import { onSchedule } from "firebase-functions/v2/scheduler";
 
-// // Start writing functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Run once a day at midnight, to clean up the users
+// Manually run the task here https://console.cloud.google.com/cloudscheduler
+exports.accountcleanup = onSchedule("every day 03:00", async (event) => {
+  const currentDate = new Date();
+  admin
+    .firestore()
+    .collection("rooms")
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        const data = doc.data();
+        const lastVoteDate = data.lastVoteTimestamp.toDate();
+        if (differenceInMinutes(currentDate, lastVoteDate) > 180) {
+          admin.firestore().collection("rooms").doc(doc.id).delete();
+        }
+      });
+    });
+  logger.log("Room cleanup finished");
+});
